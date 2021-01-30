cbcad619f608baa19d245e30d2cc154e725ec034
Camila Daniels <camila.daniels926@gmail.com>
2021-01-30 14:03:07 -0400

    Initial commit.

---
+++ src/Board.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Board extends Component {
+	render() {
+		return(
+			<div className="Board">
+				<h1>N-Queens Visualizer</h1>
+			</div>
+		)
+	}
+}
+
+export default Board;
